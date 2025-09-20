83b858dc7f1413968fa63a7a75d433d1355e000e
Prasad R Sawant <prasadsawant1618@gmail.com>
2025-09-20 20:24:25 +0530

    Create frontend/src/App.js [2025-09-20T20:24:21.509+05:30]

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'; // We will create this file later
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Your Personal Portfolio</h1>
+        <p>This is the main application component.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
