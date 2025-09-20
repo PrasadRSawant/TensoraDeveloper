4f8bb196f15fca689d3e4757b3855a1716339230
Prasad R Sawant <prasadsawant1618@gmail.com>
2025-09-20 19:54:30 +0530

    Create frontend/src/App.js. [2023-11-20]

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'; // We'll create this later
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the AI/ML Portfolio!</h1>
+        <p>
+          Explore various AI/ML models and projects.
+        </p>
+        {/* Future routes and components will go here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
